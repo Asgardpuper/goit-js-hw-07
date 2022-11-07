6c8ae8ffcc18265d59c96a119770aae4950e9ef7
Asgardpuper <maks.kachalay@gmail.com>
2022-11-08 00:55:13 +0200

    Finished Task1 and TAsk2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+
+function createElements() {
+	return galleryItems
+		.map((el) => {
+			return `<div class="gallery__item"><a class="gallery__link" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" data-source="${el.original}"
+	alt="${el.description}" />
+        </a></div>`;
+		})
+		.join("");
+}
+galleryBox.innerHTML = createElements();
+
+function getOriginakImageOnCLick(event) {
+	event.preventDefault();
+	if (!event.target.classList.contains("gallery__image")) {
+		return;
+	}
+	const image = event.target.dataset.source;
+	toggleLightBoxOnClick(image);
+}
+
+function toggleLightBoxOnClick(image) {
+	const instance = basicLightbox.create(
+		`<img width="1400" height="900" src="${image}">`
+	);
+	instance.show();
+	window.addEventListener("keydown", (event) => {
+		if (event.key === "Escape") {
+			instance.close();
+		}
+	});
+}
+galleryBox.addEventListener("click", getOriginakImageOnCLick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createElements() {
+	return galleryItems
+		.map((el) => {
+			return `<a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}" /></a>`;
+		})
+		.join("");
+}
+galleryBox.innerHTML = createElements();
+
+let test = new SimpleLightbox(".gallery a", {
+	nav: false,
+	captionsData: "alt",
+	captionDelay: 250,
+});
